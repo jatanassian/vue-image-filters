338e1a5a44f0634bc3f6a34b1f70a5c2790f9412
Julien Atanassian <julien.atanassian@gmail.com>
2024-11-07 23:49:16 -0500

    add quicksand font

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="font-['Quicksand'] max-w-xl bg-white p-8 shadow-2xl rounded absolute m-auto left-0 right-0 mt-32"
+    class="font-quicksand max-w-xl bg-white p-8 shadow-2xl rounded absolute m-auto left-0 right-0 mt-32"
   >
     <h1 class="text-center text-3xl text-indigo-700">Vue Filters</h1>
     <div
