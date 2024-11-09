833dbe6c33027c86168c16abf41a930c6ae8cef9
Julien Atanassian <julien.atanassian@gmail.com>
2024-11-08 23:13:19 -0500

    implement file upload

--- src/stores/image.ts
+++ src/stores/image.ts
@@ -9,5 +9,19 @@ export const useImageStore = defineStore('image', {
   state: (): ImageState => ({
     file: null,
     filter: ''
-  })
+  }),
+  actions: {
+    upload(event: DragEvent) {
+      if (!event.dataTransfer) {
+        return;
+      }
+      const tempFile = event.dataTransfer.files[0];
+
+      if (!tempFile.type.match('image.*')) {
+        return;
+      }
+
+      this.file = tempFile;
+    }
+  }
 });
