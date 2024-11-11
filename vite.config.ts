0ca182a84e48fce44e4bced2235afd8cf2fa7c23
Julien Atanassian <julien.atanassian@gmail.com>
2024-11-11 18:49:20 -0500

    install and setup packages for filters

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import Components from 'unplugin-vue-components/vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), Components()],
+  plugins: [vue(), vueDevTools(), Components(), wasm(), topLevelAwait()],
+  optimizeDeps: {
+    exclude: ['@silvia-odwyer/photon']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
